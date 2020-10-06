6d457c9c466a2325ca54f63f20ff42ca4cacbbe9
mab1994 <54884426+mab1994@users.noreply.github.com>
2020-10-06 13:21:59 +0200

    first commit - server side

--- Routes/locations.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const Router = express.Router()
-
-// Route: GET { api/locations } // Desc: Testing Location Routes // Access: Public
-Router.get('/', (req, res) => res.send('Location Route...'))
-
-module.exports = Router
